aec2866e24211539d965519a5f3cbac7f8a46b7a
sai srinivas <srinivas191194@msitprogram.net>
2021-03-25 23:12:09 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import axios from "axios";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 function App() {
-  const[link, setLink] = useState("");
-  const  calling = (event) => {
-    event.preventDefault()
-    let variable = document.getElementById("inputfield").value
-    console.log(variable)
-    axios.post("https://backend-urlshortner.herokuapp.com/longurl/",{"url": variable})
-         .then(function(responce){console.log(responce);  setLink(responce.data)})
-         .catch(function(error){console.log(error)})
-    }
+  const [link, setLink] = useState("");
+  const calling = (event) => {
+    event.preventDefault();
+    let variable = document.getElementById("inputfield").value;
+    console.log(variable);
+    axios
+      .post("https://backend-urlshortner.herokuapp.com/longurl/", {
+        url: variable,
+      })
+      .then(function (responce) {
+        console.log(responce);
+        setLink(responce.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   return (
     <div className="App">
-     <h1>welcome to react</h1>
-     <form method = "post" onSubmit= {calling}>
-      <label>
-        Enter URL:
-        <input type="text" id = "inputfield" name="name" placeholder ="url" required />
-      </label>
+      <h1>URL Shortner</h1>
+      <form method="post" onSubmit={calling}>
+        <label>
+          Enter URL:
+          <input
+            type="text"
+            id="inputfield"
+            name="name"
+            placeholder="url"
+            required
+          />
+        </label>
         <input type="submit" value="Submit" />
-     </form>
-     <a href = {link}>{link}</a>
+      </form>
+      <a href={link}>{link}</a>
     </div>
   );
 }
